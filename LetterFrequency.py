ac39f6a0f8d80225b703a09fe0e5b0f45441ad44
BAH0227 <brockahoover@gmail.com>
2025-02-17 21:30:59 +0000

    worked on 2-17

--- LetterFrequency.py
+++ LetterFrequency.py
@@ -9,26 +9,29 @@
 import os
 
 def countLetters(message):
-    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
     message = message.upper()
 
-    freq = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    freq = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
     #loop through each letter
+    for letter in message:
+
     #Find the position in the alphabet
     #Increase the frequency at that position. If position was 5, then frequencies[5] = frequencies[5] + 1
 
-
+        spot = (alpha.find(letter)) % 27
+        freq[spot] = freq[spot] + 1
 
     #Create the output text in the format A,5\n if there were 5 letter A in the message.
     #Remember that the \n is the symbol for a new line.
 
     output = ""
+    SpacesOfInputs = 27
     for i in range(26):
         print (alpha[i], ":", freq[i])
         line = alpha[i] + "," + str(freq[i]) + "\n"
         output = output + line
-
     writeToFile(output)
 
 
